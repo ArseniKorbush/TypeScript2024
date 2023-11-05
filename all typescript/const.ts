38927384993f68a84f43218b866f8acac68d9d4e
ArseniKorbush <122732165+ArseniKorbush@users.noreply.github.com>
2023-11-05 21:09:53 +0300

    for parameter types of const functions.ts

---
+++ all typescript/const.ts
@@ -0,0 +1,39 @@
+// Const function parameter types
+// TypeScript 5.0 added the ability to work with a type that is passed to a generic as with a literal.
+
+// Let's create a function to which we will pass an array of users.
+
+const parseUsers = <T extends {name: string, place: string}, >(users: T[]) => {
+    const getUserByName = (name: T['name']) => users.find((user) => user.name === name)
+    return getUserByName
+  }
+
+// The function returns another function, when called, we will get the user whose name we passed into it.
+
+const getUser = parseUsers([{
+    name: "Ilya",
+    place: "Krasnodar"
+  }, {
+    name: "Dmitry",
+    place: "Moscow"
+  }, {
+    name: "Pavel",
+    place: "Saint Petersburg"
+  }])
+  
+  const currentUser = getUser("Sergey") 
+
+// And the currentUser variable will be equal to undefined, since we passed the username, which is not in the users array.
+// To avoid this problem, TypeScript 5.0 added the ability to use const notation in generic.
+// To use it, just add const to the generic parseUsers function.
+
+const parseofUsers = <const T extends {name: string, place: string}, >(users: T[]) => {
+    const getUserByName = (name: T['name']) => users.find((user) => user.name === name)
+    return getUserByName
+  }
+
+// Now, when calling the getUsers function with a username that is not in the users array, TypeScript will give us an error:
+
+const currentUser = getUser("Sergey") 
+
+//  Argument of type '"Sergey"' is not assignable to parameter of type '"Ilya" | "Dmitry" | "Pavel"
