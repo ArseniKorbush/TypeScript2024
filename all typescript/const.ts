92fdbb98ca17184d105558c757f3b33a0f8561fc
ArseniKorbush <122732165+ArseniKorbush@users.noreply.github.com>
2023-11-07 23:13:30 +0300

    new features enums const.ts

--- all typescript/const.ts
+++ all typescript/const.ts
@@ -37,3 +37,41 @@ const parseofUsers = <const T extends {name: string, place: string}, >(users: T[
 const currentUser = getUser("Sergey") 
 
 //  Argument of type '"Sergey"' is not assignable to parameter of type '"Ilya" | "Dmitry" | "Pavel"
+
+// Const enums
+
+// In most cases, enumerations are a perfectly acceptable solution. However, sometimes the requirements become more stringent.
+// To avoid the cost of additional generated code and additional indirection when accessing enum values,
+// You can use const enums. Const enums are defined using the const modifier in our enums:
+
+const enum Enum {
+  A = 1,
+  B = A * 2,
+}
+
+// Const enums can only use constant enum expressions and, unlike regular enums, they are completely removed at compile time.
+// Const enumeration members are embedded in usage sites. This is possible because constant enumerations cannot have computed members.
+
+const enum Direction {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+ 
+let directions = [
+  Direction.Up,
+  Direction.Down,
+  Direction.Left,
+  Direction.Right,
+];
+
+// In the generated code it will be
+
+"use strict";
+let directions = [
+    0 /* Direction.Up */,
+    1 /* Direction.Down */,
+    2 /* Direction.Left */,
+    3 /* Direction.Right */,
+];
