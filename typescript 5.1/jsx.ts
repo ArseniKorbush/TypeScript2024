6072da4903b4dfa4dcca9168c67438d14898f0d7
ArseniKorbush <122732165+ArseniKorbush@users.noreply.github.com>
2023-11-09 23:07:37 +0300

    new features to jsx.ts

--- typescript 5.1/jsx.ts
+++ typescript 5.1/jsx.ts
@@ -15,7 +15,7 @@
 // Output will have a .js file extension. react-native mode is equivalent to preserve mode in that it preserves all JSX,
 // But the output will have a .js file extension instead.
 
-// Operator Т3416Т
+// Operator Т3416Т NEW TOPIC >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 const foo = <foo>bar;
 
@@ -27,3 +27,39 @@ const foo = <foo>bar;
 const foo = bar as foo;
 
 // The as operator is available in both .ts and .tsx files, and its behavior is identical to the angle bracket assertion style.
+
+// Type Checking NEW TOPIC >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// To understand type checking with JSX, you must first understand the difference between internal elements and value-based elements.
+// Given the JSX <expr /> expression, expr can either refer to something native to the environment
+// (e.g. a div or span in the DOM environment) or to a custom component you created. This is important for two reasons:
+
+// In React, internal elements are created as strings ( React.createElement("div") ), while the component you create is not ( React.createElement(MyComponent) ).
+// The types of attributes passed in the JSX element must be looked for differently.
+// An element's internal attributes must be known internally, whereas components will likely want to specify their own set of attributes.
+
+// TypeScript uses the same convention that React does to differentiate them.
+// An inner element always starts with a lowercase letter, and a value-based element always starts with an uppercase letter.
+
+// Intrinsic elements NEW TOPIC >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// Internal elements are viewed using a special interface, JSX.IntrinsicElements. By default, if this interface
+// Is not specified, then everything works and internal elements will not be type checked. However, if this interface is present,
+// Then the name of the inner element is treated as a property of the JSX.IntrinsicElements interface. For example:
+
+declare namespace JSX {
+    interface IntrinsicElements {
+      foo: any;
+    }
+  }
+  
+  <foo />; // ok
+  <bar />; // error
+
+// In the example above, <foo /> will work fine, but <bar /> will throw an error since it is not specified in JSX.IntrinsicElements . 
+
+declare namespace JSX {
+    interface IntrinsicElements {
+      [elemName: string]: any;
+    }
+  }
