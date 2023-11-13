cc4f9e2af41c7befc2974ed9452f9fd776996a2b
ArseniKorbush <122732165+ArseniKorbush@users.noreply.github.com>
2023-11-13 23:56:11 +0300

    new possibilities LibraryStructures.ts

---
+++ typescript 5.1/LibraryStructures.ts
@@ -0,0 +1,18 @@
+// Library Structures NEW TOPIC >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// In general terms, the way the declaration file is structured depends on how the library is used.
+// There are many ways to offer a library for consumption in JavaScript, and you will need to write a declaration file to match this.
+// This tutorial explains how to identify common library patterns and how to write declaration files that match that pattern.
+
+// Each type of basic library structuring template has a corresponding file in the Templates section.
+// You can start with these templates that will help you work faster.
+
+// Definition of Libraries types
+
+// First we'll look at the TypeScript libraries declaration file types. 
+// We'll briefly show how each type of library is used, how it's written, and give some real-world examples of libraries.
+
+// Defining the structure of a library is the first step in writing its declaration file.
+// We'll give hints on how to identify a structure based on both its usage and its code.
+// Depending on the documentation and organization of the library, one may be simpler than the other. We recommend using the one that is more convenient for you.
+
