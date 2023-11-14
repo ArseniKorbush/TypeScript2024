d474415b839608f17d8792f59886d007bdf06844
ArseniKorbush <122732165+ArseniKorbush@users.noreply.github.com>
2023-11-14 21:33:00 +0300

    templates for modules to LibraryStructures.ts

--- typescript 5.1/LibraryStructures.ts
+++ typescript 5.1/LibraryStructures.ts
@@ -21,19 +21,18 @@
 // A question to ask yourself when looking at the library you are trying to print.
 
 //      How to get the library?
-
 //      For example, is it possible to get it only through npm or only from CDN?
-
 //      How would you import it?
-
 //      Does it add a global object? Are require or import/export statements used?
 
 // Smaller samples for different types of T1721T
 // Modular Libraries
 
-// Almost every modern Node.js library falls into a family of modules. These types of libraries only work in a JS environment with a module loader. For example, express only works in Node.js and must be loaded using the CommonJS require function.
+// Almost every modern Node.js library falls into a family of modules. These types of libraries only work
+// in a JS environment with a module loader. For example, express only works in Node.js and must be loaded using the CommonJS require function.
 
-// ECMAScript 2015 (also known as ES2015, ECMAScript 6 and ES6), CommonJS and RequireJS have similar module import ideas. In JavaScript CommonJS (Node.js), for example, you would write
+// ECMAScript 2015 (also known as ES2015, ECMAScript 6 and ES6), CommonJS and RequireJS have similar module import ideas.
+//  In JavaScript CommonJS (Node.js), for example, you would write
 
 var fs = require("fs");
 
@@ -52,3 +51,38 @@ define(..., ['someLib'], function(someLib) {
 });
 
 // As with global modules, you can see these examples in the documentation of a UMD module, so be sure to check the code or documentation.
+
+// Identifying a Library module by code NEW TOPIC >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// Modular libraries usually have at least some of the following characteristics:
+
+// Unconditional calls to require or define
+// Declarations like import * as a from 'b'; or export c;
+// Destinations for exports or module.exports
+
+// They rarely have:
+
+// Assigning window or global properties
+
+// Templates for modules
+
+// There are four templates available for modules: module.d.ts, module-class.d.ts, module-function.d.ts and module-plugin.d.ts.
+
+// You should read module.d.ts first to get a general idea of how they all work.
+
+// Then use the module-function.d.ts template if your module can be called as a function:
+
+const x = require("foo");
+// Note: call 'x' as a function
+const y = x(42);
+
+// Use the module-class.d.ts template if your module can be built using new :
+
+const x = require("bar");
+// Note: using the new operator on an imported variable
+const y = new x("hello");
+
+// If you have a module that makes changes to other modules when imported, use the module-plugin.d.ts template:
+
+const jest = require("jest");
+require("jest-matchers-files");
